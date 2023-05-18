7169dab16a38498ba448d5b3b577e0accb3a57a6
Cedrus81 <erez.eitan.97@gmail.com>
2023-05-18 14:55:33 +0300

    updated redo/undo panel UI

--- client/src/views/composer/StageUI.tsx
+++ client/src/views/composer/StageUI.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useState } from 'react';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Box from '@material-ui/core/Box';
-import { IconButton, Modal } from '@material-ui/core';
+import { IconButton, Modal, Typography } from '@material-ui/core';
 import TuneIcon from '@material-ui/icons/Tune';
 import RedoIcon from '@material-ui/icons/Redo';
 import UndoIcon from '@material-ui/icons/Undo';
@@ -19,6 +19,8 @@ export interface StageUIProps {
 }
 
 export const StageUI = ({ score, onUpdateScore, onRedoUndo }: StageUIProps) => {
+	const pageWidth = 776;
+	const stageWidth = 718;
 	const useStyles = makeStyles(() => ({
 		root: {
 			position: 'relative',
@@ -48,36 +50,74 @@ export const StageUI = ({ score, onUpdateScore, onRedoUndo }: StageUIProps) => {
 			color: '#000',
 		},
 		scoreOptions: {
-			position: 'fixed',
+			position: 'absolute',
+			display: 'flex',
+			width: pageWidth - 16 - (pageWidth - stageWidth) / 2,
 			left: 16,
 			top: 16,
 		},
 		scoreOptionButton: {
 			width: '24px !important',
-			height: '24px !important',
+			height: '32px !important',
 			textAlign: 'center',
 			cursor: 'pointer',
 			transition: 'all 0.2s ease-in-out',
 			color: '#333',
-			'&:hover': {
-				color: '#000',
-			},
 			'&.disabled': {
 				pointerEvents: 'none',
-				color: '#999',
+				color: '#444',
 			},
 			'@media print': {
 				display: 'none',
 			},
 		},
-		tuneButton: {
-			marginInlineEnd: '674px',
+		undoRedoPanel: {
+			display: 'flex',
+			alignItems: 'center',
+			backgroundColor: '#222',
+			padding: 4,
+			borderRadius: 4,
+			boxShadow: '3px 3px 2px rgba(0, 0, 0, 0.2)',
+		},
+		history: {
+			color: '#ccc',
+			backgroundColor: '#444',
+			padding: 4,
+		},
+		undoBtn: {
+			borderRadius: 0,
+			borderTopLeftRadius: 4,
+			borderBottomLeftRadius: 4,
+			'&.disabled':{
+				borderInlineEnd: '6px solid black',
+			},
+			'& svg': {
+				fontSize: 21,
+				color: 'white',
+				transform: 'rotate(34deg)',
+			},
+		},
+		redoBtn: {
+			borderRadius: 0,
+			borderTopRightRadius: 4,
+			borderBottomRightRadius: 4,
+			'&.disabled':{
+				borderInlineStart: '6px solid black',
+			},
+			'& svg': {
+				fontSize: 21,
+				color: 'white',
+				transform: 'rotate(-34deg)',
+			},
+		},
+		tuneBtn: {
+			flex: 1,
+			display: 'flex',
+			justifyContent: 'start',
+			// marginInlineEnd: '674px',
 		},
 	}));
 	const classes = useStyles();
-
-	const pageWidth = 776;
-	const stageWidth = 718;
 
 	const [tuneStageDialogVisible, setTuneStageDialogVisible] = useState(false);
 
@@ -103,23 +143,25 @@ export const StageUI = ({ score, onUpdateScore, onRedoUndo }: StageUIProps) => {
 		setTuneStageDialogVisible(false);
 	}, []);
 
-
-
 	return (
 		<>
 			{score && (
 				<Box id="StageUI" className={`${classes.root} no-scrollbar`} style={{ width: `${pageWidth}px`, padding: `${(pageWidth - stageWidth) / 2}px` }}>
 					<Box className={classes.content} style={{ width: `${stageWidth}px` }}>
 						<Box className={classes.scoreOptions}>
-							<IconButton onClick={handleClickTune} className={`${classes.scoreOptionButton} ${classes.tuneButton}`} disabled={!score}>
+							<IconButton onClick={handleClickTune} className={`${classes.scoreOptionButton} ${classes.tuneBtn}`} disabled={!score}>
 								<TuneIcon titleAccess="Tune Page" />
 							</IconButton>
-							<IconButton onClick={()=> onRedoUndo(-1)} className={`${classes.scoreOptionButton}`} disabled={!score}>
-								<UndoIcon titleAccess="Undo" />
-							</IconButton>
-							<IconButton onClick={()=> onRedoUndo(1)} className={`${classes.scoreOptionButton}`} disabled={!score}>
-								<RedoIcon titleAccess="Redo" />
-							</IconButton>
+							{/* todo: move this panel to a separate component */}
+							<Box className={classes.undoRedoPanel}>
+								<IconButton onClick={() => onRedoUndo(-1)} className={`${classes.scoreOptionButton} ${classes.undoBtn}`} disabled={!score}>
+									<UndoIcon titleAccess="Undo" />
+								</IconButton>
+								<Typography className={`${classes.history}`}>2/5</Typography>
+								<IconButton onClick={() => onRedoUndo(1)} className={`${classes.scoreOptionButton} ${classes.redoBtn}`} disabled={!score}>
+									<RedoIcon titleAccess="Redo" />
+								</IconButton>
+							</Box>
 						</Box>
 						<Modal open={tuneStageDialogVisible} onClose={handleCloseTuneStageDialog}>
 							<TunePageDialog score={score} onUpdateScore={handleScoreUpdated} onDoneTuneStageDialog={handleDoneTuneStageDialog} />
